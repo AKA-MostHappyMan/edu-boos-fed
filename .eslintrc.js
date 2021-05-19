25f3fcc37445a211c2dacd55257abb925c79a387
zhang <zhangshaohong1997@gmail.com>
2021-05-19 20:38:40 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  // 使用插件的编码校验规则
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  // 自定义编码校验规则
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>zhang</h1>
+    <!-- 根路由的出口 -->
+    <router-view />
+  </div>
+</template>
+
+
+<script lang="ts">
+import Vue from "vue";
+// 1. 编辑器给的类型提示
+// 2. TypeScript 编译期间的类型验证
+
+export default Vue.extend({});
+</script>
+
+
+
+
+
+<style lang="scss" scoped>
+</style>
